b1a2e32752fcfbb60b75765901ebf1730120d77a
Feras Ahmed <ferasahmed@ferass-macbook-pro-2.local>
2015-11-15 15:14:43 -0500

    Initial Commit

---
+++ Take Home Assignment/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Take Home Assignment
+//
+//  Created by Feras Ahmed on 11/15/15.
+//  Copyright © 2015 Feras Ahmed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
